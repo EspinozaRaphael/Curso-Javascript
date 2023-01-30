4a6fb348d31108c448a4243c3d6681762dc3f753
Deus <espinozaedu1998@gmail.com>
2023-01-30 16:48:50 -0300

    segundo commit

---
+++ Variables.js
@@ -0,0 +1,28 @@
+var cajon1;
+var cajon1 = "Guantes Azules";
+//string
+var bootcamp = "Henry";
+// numeros
+var numeroEntero =17;
+var numeroNegativo =-4;
+var numeroDecimal = 11.75;
+//Booleanos
+
+var estoyEntendiendo = true;
+var estoyAburrido = False;
+
+//undefine
+
+var cajon1;
+console.log(cajon1);
+
+//Null
+
+var sinNada =null;
+
+
+
+
+
+
+
